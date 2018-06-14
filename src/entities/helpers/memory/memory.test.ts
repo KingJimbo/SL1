57a6990078ec975f593cbeebec789f83e7da0a6a
Fraser Wright <fraser.wright@eqtr.com>
2018-06-14 09:03:09 +0100

    Unit Testing1

--- src/entities/helpers/memory/memory.test.ts
+++ src/entities/helpers/memory/memory.test.ts
@@ -3,15 +3,36 @@ import MemoryHelper from './memory';
 //import { expect } from 'chai';
 var chai = require('chai');
 var expect = chai.expect;
+// var sinon = require('ts-sinon');
+// var stubObject = sinon.stubObject;
 //import 'mocha';
+
+import * as sinon from "ts-sinon";
+const stubObject = sinon.stubObject;
+const stubInterface = sinon.stubInterface;
+
+
 
 describe('cleanUpDeadCreeps', () => {
 
   it('should remove any creep memory', () => {
-    let memory = new Memory();
-    Memory.creeps = {};
-    Memory.creeps["Creep1"] = { id: 1};
-    Game.creeps["Creeps2"] = { id: 2};
+    const creepStub = stubInterface<Creep>();
+
+    const memoryStub = stubInterface<Memory>();
+    const gameStub = stubInterface<Game>();
+
+    expect(memoryStub.creeps).to.be.undefined;
+    memoryStub.creeps = {
+      "Creep1":{ id: 1},
+      "Creep2":{ id: 2}
+    };
+
+    gameStub.creeps = [
+
+      {
+      "Creep2":{ id: 2}
+    }]
+
     let memoryHelper = new MemoryHelper();
     memoryHelper.cleanDeadCreeps();
     expect(result).to.equal('Hello world!');
